926a0f69275eeaf6555ed8c42eebab154e10a6ae
Riyusora <xiaofeng2332@gmail.com>
2025-11-03 01:31:21 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 顶栏标题文本
-		text: "MizukiUI",
+		text: "Riyusoraのblog",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 	},
